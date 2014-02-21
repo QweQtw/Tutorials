40bc3081a2a4726b0139263acfa4c1451a4f244f
QweQtw <t_a_w@onet.eu>
2014-02-21 14:37:46 +0100

    LoginTest
    2014.02.21

--- JAVA/JavaFX/Login/src/login/Login.java
+++ JAVA/JavaFX/Login/src/login/Login.java
@@ -7,8 +7,15 @@ package login;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.StrokeType;
 import javafx.stage.Stage;
 
 /**
@@ -19,19 +26,41 @@ public class Login extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        GridPane mainGrid = new GridPane();
-        mainGrid.setAlignment(Pos.CENTER);
-        mainGrid.setHgap(8);
-        mainGrid.setVgap(8);
-        mainGrid.setPadding(new Insets(16,16,16,16));
         
-        Scene scene = new Scene(mainGrid, 320, 200);
+        GridPane grid = getGrid();
+        grid.add(new Label("User Name:"), 0, 1);
+        grid.add(new TextField(), 1, 1);
+        grid.add(new Label("Password:"), 0,2);
+        grid.add(new PasswordField(),1,2);
+        Group root = new Group();
+        
+        Group circles = new Group();
+        for (int i = 0; i < 30; i++) {
+   Circle circle = new Circle(100+i*10, Color.web("black", 0.05));
+   circle.setStrokeType(StrokeType.OUTSIDE);
+   circle.setStroke(Color.web("black", 0.16));
+   circle.setStrokeWidth(4);
+   circles.getChildren().add(circle);
+    }
+        root.getChildren().add(circles);
+        grid.add(root, 0, 3);
+
+        Scene scene = new Scene(grid, 320, 200);
         
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+    private GridPane getGrid()
+    {
+        GridPane mainGrid = new GridPane();
+        mainGrid.setAlignment(Pos.CENTER);
+        mainGrid.setHgap(8);
+        mainGrid.setVgap(8);
+        mainGrid.setPadding(new Insets(16,16,16,16));
+        return mainGrid;
+    }
     /**
      * The main() method is ignored in correctly deployed JavaFX application.
      * main() serves only as fallback in case the application can not be
